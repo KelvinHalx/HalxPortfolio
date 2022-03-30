601b20f3a5f6c98babd9ffc96b9c5554f66d4394
Kelvin Halx <kelvinwainaina03@gmail.com>
2022-03-30 13:27:37 +0300

    Structure Backend and Frontend

--- App/routes.py
+++ App/routes.py
@@ -1,7 +1,6 @@
-from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
 
 # Entire Site
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('front_end/index.html')
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from App import app
+from App import routes
 
 if __name__ == '__main__':
     app.run(debug=True)
