741e47094ac04446f16ad58844ab2b9bdf673e08
Kelvin Halx <kelvinwainaina03@gmail.com>
2022-03-31 19:57:20 +0300

    Routing

--- App/routes.py
+++ App/routes.py
@@ -1,6 +1,6 @@
-from flask import render_template, url_for, flash, redirect, request, abort
+# from flask import render_template, url_for, flash, redirect, request, abort
 
-# Entire Site
-@app.route('/')
-def index():
-    return render_template('front_end/index.html')
+# # Entire Site
+# @app.route('/')
+# def index():
+#     return render_template('front_end/index.html')
--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from App import routes
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Here goes nothing"
 
 if __name__ == '__main__':
     app.run(debug=True)
